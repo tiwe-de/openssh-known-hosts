4b444020fa50e4f22a427b981c582b35cdb4efb1
Timo Weingärtner <timo@tiwe.de>
2012-02-25 21:17:00 +0100

    Imported Debian version 0.2

--- debian/openssh-known-hosts.cron.d
+++ debian/openssh-known-hosts.cron.d
@@ -1,4 +1,4 @@
 #
 # Regular cron jobs for the openssh-known-hosts package
 #
-0 *	* * *	root	[ -x /usr/share/openssh-known-hosts/update-known-hosts ] && /usr/share/openssh-known-hosts/update-known-hosts
+0 *	* * *	root	[ -x /usr/sbin/update-openssh-known-hosts ] && /usr/sbin/update-openssh-known-hosts
