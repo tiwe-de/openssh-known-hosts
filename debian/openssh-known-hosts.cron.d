d61655fd3e045038fb337e5e40f6b04dec6b5cd9
Timo Weingärtner <timo@tiwe.de>
2013-01-23 18:13:35 +0100

    add random sleep to cron.d file to reduce DDoS effects on servers

--- debian/openssh-known-hosts.cron.d
+++ debian/openssh-known-hosts.cron.d
@@ -1,4 +1,4 @@
 #
 # Regular cron jobs for the openssh-known-hosts package
 #
-0 *	* * *	root	[ -x /usr/sbin/update-openssh-known-hosts ] && /usr/sbin/update-openssh-known-hosts
+0 *	* * *	root	[ -x /usr/sbin/update-openssh-known-hosts ] && { sleep `od -An -N1 -tu1 /dev/urandom`; /usr/sbin/update-openssh-known-hosts; }
